c6c73b5857bed384419233d7ed406a0b84085c96
ochukooh <74387488+ochukooh@users.noreply.github.com>
2024-07-13 21:50:33 +0100

    Update Lucky_shrub database_developing functions.sql
    procedure to estimate cost after discount has been added

--- Lucky_shrub database_developing functions.sql
+++ Lucky_shrub database_developing functions.sql
@@ -36,3 +36,24 @@ INSERT INTO Orders(OrderID, ClientID, ProductID , Quantity, Cost, Date) VALUES
 CREATE FUNCTION FindCost(order_id INT) 
 RETURNS DECIMAL (5,2) DETERMINISTIC 
 RETURN (SELECT Cost FROM Orders WHERE OrderID = order_id);
+--stored procedure that return the final cost of the customer’s order after the discount value has been deducted.
+--if order quantity is more than 20 returns a 20% discount, if 10 returns 10% discount
+DELIMITER // 
+
+CREATE Procedure GetDiscount(OrderIDInput INT) 
+     BEGIN 
+         DECLARE cost_after_discount DECIMAL(7,2); 
+         DECLARE current_cost DECIMAL(7,2); 
+         DECLARE order_quantity INT; 
+         SELECT Quantity INTO order_quantity FROM Orders WHERE OrderID = OrderIDInput; 
+         SELECT Cost INTO current_cost FROM Orders WHERE OrderID = OrderIDInput; 
+        IF order_quantity >= 20 THEN
+          SET cost_after_discount = current_cost - (current_cost * 0.2);              
+        ELSEIF order_quantity >= 10 THEN
+          SET cost_after_discount = current_cost - (current_cost * 0.1); 
+        ELSE SET cost_after_discount = current_cost;
+        END IF;
+    SELECT cost_after_discount; 
+END//
+
+DELIMITER ; 
